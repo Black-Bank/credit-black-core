cc0c7440b394fd8685cac0601e9c9178c913b542
Cyro MBLabs <cyroMBlabs@NMB-DESENV-421.local>
2024-05-25 00:52:44 -0300

    render, connection

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Server is running on http://localhost:${port}`);
 }
+
 bootstrap();
